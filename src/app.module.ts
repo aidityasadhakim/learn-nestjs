5e1f87699ea60a123ee3fddd924b9d313da98200
aidityas <aidityasadhakim250@gmail.com>
2023-05-08 12:21:40 +0700

    Hide mongodb URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,22 @@ import { TodosModule } from './todos/todos.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
     UsersModule,
     TodosModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://aidityasadhakim250:e95mfBW73exIniwc@ecommerce-api.hu05vy5.mongodb.net/?retryWrites=true&w=majority',
-      {
-        dbName: 'ewallet_database',
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      dbName: 'ewallet_database',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     AuthModule,
+    TransactionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
